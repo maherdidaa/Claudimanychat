fc9214011b418d556625877d5e9fa98e8a123714
maherdidaa <didanada20202@gmail.com>
2026-07-14 06:40:23 +0300

    Add files via upload

---
+++ web/{lib/app/(dashboard)/automations/page.tsx
@@ -0,0 +1,9 @@
+import { AutomationList } from '@/components/automations/AutomationList';
+
+export default function AutomationsPage() {
+  return (
+    <main className="p-8">
+      <AutomationList />
+    </main>
+  );
+}
